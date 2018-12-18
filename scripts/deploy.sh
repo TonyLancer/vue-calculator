a6b03b8a546a836a92eff2fdf1d05f299aa9b8c9
Tony Lancer <tony12lancer@gmail.com>
2018-12-18 14:09:15 +0200

    Changed URL for deploying

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,6 +21,7 @@ git commit -m 'deploying website'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:TonyLancer/vue-calculator.git master:gh-pages
+# git push -f git@github.com:TonyLancer/vue-calculator.git master:gh-pages
+git push -f https://github.com/TonyLancer/vue-calculator.git master:gh-pages
 
 cd -
